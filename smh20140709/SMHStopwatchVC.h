33b5e3a85408ecd303e93b5a89bb23f307583012
smh20140704 <smh20140704@gmail.com>
2014-07-09 15:39:13 +0100

    UI Design

---
+++ smh20140709/SMHStopwatchVC.h
@@ -0,0 +1,13 @@
+//
+//  SMHStopwatchVC.h
+//  smh20140709
+//
+//  Created by Shah Hussain on 09/07/2014.
+//  Copyright (c) 2014 smh20140704. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMHStopwatchVC : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ smh20140709/SMHStopwatchVC.m
@@ -0,0 +1,69 @@
+//
+//  SMHStopwatchVC.m
+//  smh20140709
+//
+//  Created by Shah Hussain on 09/07/2014.
+//  Copyright (c) 2014 smh20140704. All rights reserved.
+//
+
+#import "SMHStopwatchVC.h"
+
+@interface SMHStopwatchVC ()
+
+@end
+
+@implementation SMHStopwatchVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SMHStopwatchLapCell"];
+    
+    // Configure the cell...
+
+    return cell;
+}
+
+@end
